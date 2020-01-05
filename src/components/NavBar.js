1330391a0e05521d6640dddf3a5c2d090d2cb551
Daniel Dawson <dannyldawson@gmail.com>
2020-01-04 18:52:30 -0800

    Create basic navbar
    Will need to go back and rethink what links are necessary, and change
    branding to reflect app name/colors.

---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { ReactComponent as Logo } from "../remote_oasis_icon.svg";
+import { Link, NavLink } from "react-router-dom";
+
+function NavBar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      <Navbar
+        expanded={isExpanded}
+        onToggle={setIsExpanded}
+        onSelect={() => setIsExpanded(false)}
+        expand="md"
+        fixed="top"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand as={Link} to="/">
+          <Logo className="mr-3" />
+          Navbar
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link eventKey="home" as={NavLink} to="/" exact>
+              Home
+            </Nav.Link>
+            <Nav.Link eventKey="explore" as={NavLink} to="/explore">
+              Explore
+            </Nav.Link>
+            <Nav.Link eventKey="about" as={NavLink} to="/about">
+              About
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
