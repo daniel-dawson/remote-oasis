1501643ce06d8c2f1a8f9583427e0526abeb8580
Daniel Dawson <dannyldawson@gmail.com>
2020-01-05 20:14:17 -0800

    fix apollo-link problems

--- src/index.js
+++ src/index.js
@@ -6,7 +6,50 @@ import App from "./app/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+
+import gql from "graphql-tag";
 import * as serviceWorker from "./serviceWorker";
+
+const cache = new InMemoryCache();
+const httpLink = new createHttpLink({
+  uri: "https://api.yelp.com/v3/graphql"
+});
+
+console.log(httpLink);
+
+const client = new ApolloClient({
+  cache,
+  link: httpLink,
+  fetchOptions: {
+    mode: "no-cors"
+  }
+});
+
+client
+  .query({
+    query: gql`
+      {
+        search(term: "cafe", location: "seattle") {
+          business {
+            name
+            rating
+            location {
+              address1
+              city
+              state
+              country
+            }
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
 
 ReactDOM.render(
   <Router>
