d877efd218f0eb80ffdcab6d9f01af73c8b18325
Daniel Dawson <dannyldawson@gmail.com>
2020-01-07 21:50:38 -0800

    Add search form

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CafeSearchForm from "../features/cafeSearch/CafeSearchForm";
 
 export default function Explore() {
   return (
     <div>
       <h1>Explore</h1>
+      <CafeSearchForm />
     </div>
   );
 }
