85eced20018ead844023706ff361e5a5a468ef87
Daniel Dawson <dannyldawson@gmail.com>
2020-01-24 02:12:23 -0800

    Import reducers, and combine

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,5 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({});
+import cafesDisplayReducer from "../features/displayCafes/cafesDisplaySlice";
+import cafesListReducer from "../features/cafesList/cafesSlice";
+
+const rootReducer = combineReducers({
+  cafesDisplay: cafesDisplayReducer,
+  cafes: cafesListReducer
+});
 
 export default rootReducer;
