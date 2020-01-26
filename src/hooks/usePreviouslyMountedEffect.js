e18a823a7865265c8dd9b5c6637e57c7ba79bbda
Daniel Dawson <dannyldawson@gmail.com>
2020-01-25 16:09:24 -0800

    Add comments

--- src/hooks/usePreviouslyMountedEffect.js
+++ src/hooks/usePreviouslyMountedEffect.js
@@ -1,5 +1,9 @@
 import { useEffect, useRef } from "react";
 
+// Will do nothing the first time a component is mounted
+// On subsequent mounts, will invoke func
+// @param func: function
+// @param deps: Array<dependencies>
 export const usePreviouslyMountedEffect = (func, deps) => {
   const isInitialMount = useRef(true);
   useEffect(() => {
