7366600d09801698414bc80ab8e6d40b90c9a2df
Daniel Dawson <dannyldawson@gmail.com>
2020-02-27 08:18:07 -0800

    Add favorites reducer to root

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import cafesDisplayReducer from "../features/displayCafes/cafesDisplaySlice";
 import cafesListReducer from "../features/cafesList/cafesSlice";
+import favoritesReducer from "../features/favorites/favorites";
 
 const rootReducer = combineReducers({
   cafesDisplay: cafesDisplayReducer,
-  cafes: cafesListReducer
+  cafes: cafesListReducer,
+  favorites: favoritesReducer
 });
 
 export default rootReducer;
