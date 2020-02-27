7f06fd92de9712e4d7ad86a629ceb89ff628d935
Daniel Dawson <dannyldawson@gmail.com>
2020-02-27 08:23:46 -0800

    Convert to react-bootstrap card

--- src/features/cafesList/Cafe.js
+++ src/features/cafesList/Cafe.js
@@ -1,17 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-function Cafe({ cafe, showCafe }) {
+function Cafe({ cafe, showCafe, favoritesById, recommendCafe }) {
+  const isRecommended = favoritesById.hasOwnProperty(cafe.id);
   return (
-    <div>
-      <h3>
-        <button onClick={() => showCafe(cafe.id)}>{cafe.name}</button>
-      </h3>
-      <p>Avg rating: {cafe.rating}</p>
-      <p>
-        Location: {cafe.location.address1}, {cafe.location.city},{" "}
-        {cafe.location.state}
-      </p>
-    </div>
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          <Link to={`cafes/${cafe.id}`} onClick={() => showCafe(cafe.id)}>
+            {cafe.name}
+          </Link>
+        </Card.Title>
+        <Card.Text>
+          Avg rating: {cafe.rating}
+          <br />
+          Location: {cafe.location.address1}, {cafe.location.city},{" "}
+          {cafe.location.state}
+        </Card.Text>
+        {!isRecommended && (
+          <Button onClick={e => recommendCafe(e, cafe)} variant="primary">
+            <span className="glyphicon glyphicon-heart-empty"></span> Recommend
+          </Button>
+        )}
+      </Card.Body>
+      {isRecommended && (
+        <Card.Footer>
+          <small className="text-muted">
+            <span className="glyphicon glyphicon-heart"></span> Recommended
+          </small>
+        </Card.Footer>
+      )}
+    </Card>
   );
 }
 
