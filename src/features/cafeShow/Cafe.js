49fd3f1aceac9df69f7d2a72b1300a0d1a0886e6
Daniel Dawson <dannyldawson@gmail.com>
2020-02-27 08:22:51 -0800

    Convert to react-bootstrap card

--- src/features/cafeShow/Cafe.js
+++ src/features/cafeShow/Cafe.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
 function Cafe({ cafe }) {
-  const { name, price, rating } = cafe;
+  if (cafe === undefined) {
+    return <Redirect to="/explore/cafes" />;
+  }
+  const { name, location, price, rating } = cafe;
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>Price: {price}</p>
-      <p>Rating: {rating}</p>
-    </div>
+    <Card>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          Avg rating: {rating}
+          <br />
+          Price: {price}
+          <br />
+          Location: {cafe.location.address1}, {location.city},{" "}
+          {cafe.location.state}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">some info/button here</small>
+      </Card.Footer>
+    </Card>
   );
 }
 
